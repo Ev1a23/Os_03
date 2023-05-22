241c53cb4d673b14e10d37df183b53daaaa89372
Ev1a23 <eviatars@mail.tau.ac.il>
2023-05-22 18:26:50 +0300

    lil fixes

--- message_reader.c
+++ message_reader.c
@@ -1,4 +1,4 @@
-#include <errno.h> /* error codes */
+#include <errno.h> 
 #include "message_slot.h"
 #include <fcntl.h>
 #include <sys/ioctl.h>
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <linux/fs.h>
 
 int main (int argc, char** argv)
 {
--- message_sender.c
+++ message_sender.c
@@ -1,50 +1,42 @@
-#undef __KERNEL__
-#define __KERNEL__
-#undef MODULE
-#define MODULE
-
-
-#include <linux/kernel.h>   /* We're doing kernel work */
-#include <linux/module.h>   /* Specifically, a module */
-#include <linux/fs.h>       /* for register_chrdev */
-#include <linux/string.h>   /* for memset. NOTE - not string.h!*/
-#include <errno.h> /* error codes */
+#include <errno.h> 
 #include "message_slot.h"
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <linux/fs.h>
 
 int main (int argc, char** argv)
 {
     if(argc !=4)
     {
-        strerror(errno);
-        exit(1);
+        error(strerror(errno));
     }
     int fd = open(argv[1], O_RDONLY);
     if(fd < 0)
     {
-        strerror(errno);
-        exit(1);
+        error(strerror(errno));
     }
-    int channel_id = atoi(argv[2]);
+    long channel_id = strtoul(argv[2], NULL, 0);
     if(ioctl(fd, MSG_SLOT_CHANNEL, channel_id) < 0)
     {
-        strerror(errno);
-        exit(1);
+        error(strerror(errno));
     }
-    ssize_t len = len_msg(argv[3]);
+    size_t len = len_msg(argv[3]);
     if(write(fd, argv[3], len)<0)
     {
-        strerror(errno);
-        exit(1);
+        error(strerror(errno));
     }
     if(close(fd)<0)
     {
-        strerror(errno);
-        exit(1);
+        error(strerror(errno));
     }
     exit(0);
 }
 
-ssize_t len_msg(char* msg)
+size_t len_msg(char* msg)
 {
     ssize_t len = 0;
     while(msg[len] != '\0')
@@ -52,4 +44,10 @@ ssize_t len_msg(char* msg)
         ++len;
     }
     return len;
+}
+
+void error(char *msg)
+{
+    fprintf(stderr, "%s", msg);
+    exit(1);
 }
