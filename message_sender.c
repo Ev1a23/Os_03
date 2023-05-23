b4dba1f99b0c3c449258d148d7352bdeaf17e512
Ev1a23 <eviatars@mail.tau.ac.il>
2023-05-23 10:43:59 +0300

    change to write only

--- message_sender.c
+++ message_sender.c
@@ -17,7 +17,7 @@ int main (int argc, char** argv)
     {
         error(strerror(errno));
     }
-    int fd = open(argv[1], O_RDONLY);
+    int fd = open(argv[1], O_WRONLY);
     if(fd < 0)
     {
         error(strerror(errno));
