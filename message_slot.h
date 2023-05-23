09ab7ea785c700f1de4a5ad98d912e6bfb067a27
Ev1a23 <eviatars@mail.tau.ac.il>
2023-05-23 15:10:41 +0300

    header

--- message_slot.h
+++ message_slot.h
@@ -1,8 +1,3 @@
-// The major device number.
-// We don't rely on dynamic registration
-// any more. We want ioctls to know this
-// number at compile time.
-//#define MAJOR_NUM 235
 #define MAJOR_NUM 235
 #define DEVICE_RANGE_NAME "char_dev"
 #define BUF_LEN 128
