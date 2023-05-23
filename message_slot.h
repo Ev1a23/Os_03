c31d4e5ab071bf501ed7004facd965faad1769f1
Ev1a23 <eviatars@mail.tau.ac.il>
2023-05-23 15:08:05 +0300

    fix for header

--- message_slot.h
+++ message_slot.h
@@ -1,16 +1,12 @@
-#ifndef MESSAGE_SLOT_H
-#define MESSAGE_SLOT_H
-
 // The major device number.
 // We don't rely on dynamic registration
 // any more. We want ioctls to know this
 // number at compile time.
 //#define MAJOR_NUM 235
 #define MAJOR_NUM 235
-#define DEVICE_RANGE_NAME "message_slot"
+#define DEVICE_RANGE_NAME "char_dev"
 #define BUF_LEN 128
 #define SUCCESS 0
 #define DEVICE_FILE_NAME "message_slot"
 #define MSG_SLOT_CHANNEL 3
 
-#endif
